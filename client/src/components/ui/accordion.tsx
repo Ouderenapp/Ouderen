0f6afeb7960792bf7d5b3ed29d1e6c74e67d4db7
wessie7631 <40059443-wessie7631@users.noreply.replit.com>
2025-02-27 20:35:35 +0000

    Refactor: Simplify Accordion component styling

--- client/src/components/ui/accordion.tsx
+++ client/src/components/ui/accordion.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDown } from "lucide-react"
-
 import { cn } from "@/lib/utils"
 
 const Accordion = AccordionPrimitive.Root
@@ -44,10 +43,7 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className={cn(
-      "overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
-      className
-    )}
+    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
     <div className="pb-4 pt-0">{children}</div>
