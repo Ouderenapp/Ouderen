a4d2864e59415be53e03bba89da03500c4dfd464
Wessel Kastelijn <w.kastelijn@student.fontys.nl>
2025-03-17 11:50:21 +0100

    Voeg Vercel configuratie toe voor correcte serverless deployment

---
+++ server/util.ts
@@ -0,0 +1,16 @@
+/**
+ * Utility functies voor de server
+ */
+
+/**
+ * Log een bericht met tijdstempel en bron
+ */
+export function log(message: string, source: string = 'express') {
+  const formattedTime = new Date().toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  });
+  console.log(`${formattedTime} [${source}] ${message}`);
+} 
