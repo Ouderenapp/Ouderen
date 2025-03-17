e916e9213598be88339a5c3a6fc60eb7a69e35dc
Wessel Kastelijn <w.kastelijn@student.fontys.nl>
2025-03-17 11:55:21 +0100

    Fix TypeScript type voor Vite serverOptions in Vercel deployment

--- server/vite.ts
+++ server/vite.ts
@@ -9,6 +9,13 @@ import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
 
+// Definieer een type voor serverOptions dat compatibel is met Vercel
+type ViteServerOptions = {
+  middlewareMode: boolean;
+  hmr: { server: Server };
+  allowedHosts: true | string[]; // Expliciet type dat alleen true of string[] toestaat
+};
+
 const viteLogger = createLogger();
 
 export function log(message: string, source = "express") {
@@ -23,7 +30,7 @@ export function log(message: string, source = "express") {
 }
 
 export async function setupVite(app: Express, server: Server) {
-  const serverOptions = {
+  const serverOptions: ViteServerOptions = {
     middlewareMode: true,
     hmr: { server },
     allowedHosts: true,
